5b3eccd142a29d52f77ac66cf47c662ac8f46b6d
Hamid Abrar Mahir <61085727+MahirAbrar@users.noreply.github.com>
2024-09-09 16:33:13 +1000

    Time: 152 ms (79.02%), Space: 16.5 MB (79.25%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -3,19 +3,14 @@ class Solution:
         res = 0
 
         for middle in range(len(s)):
+            res += self.isPali(s, middle, middle)
+            res += self.isPali(s, middle, middle + 1)
+        return res
 
-            l, r = middle, middle
-            
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                res += 1
-                l -= 1
-                r += 1
-
-            l, r = middle, middle + 1
-            
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                res += 1
-                l -= 1
-                r += 1
-        
+    def isPali(self, s, l, r):
+        res = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            res += 1
+            l -= 1
+            r += 1
         return res
