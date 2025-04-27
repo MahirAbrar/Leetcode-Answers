ddc2058f230ae74da350fbd793db32d20dd61be0
Hamid Abrar Mahir <61085727+MahirAbrar@users.noreply.github.com>
2025-04-28 00:24:44 +0600

    Time: 35 ms (25.84%), Space: 18.4 MB (10.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,17 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
+        anagrams = defaultdict(list)
 
         for s in strs:
-            count = [0] * 26
-
+            base = [0] * 26
             for c in s:
-                count[ord(c) - ord("a")] += 1
-            res[tuple(count)].append(s)
-        return res.values()
+                code = ord(c) - ord("a")
+                base[code] = base[code] + 1
+            anagrams[tuple(base)].append(s)
+
+        return list(anagrams.values())    
